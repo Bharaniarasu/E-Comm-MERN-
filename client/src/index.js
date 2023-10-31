ecfc8ae8df59ae339caa0b81f071ce5111d446b9
Bharaniarasu <bharaniarasu.t@gmail.com>
2023-11-01 03:26:48 +0530

    proxy changed

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 import store from "./components/redux/store/store";
 import { Provider } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.js";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
