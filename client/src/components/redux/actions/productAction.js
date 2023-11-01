f2773c2ac93269f115c09ac248ef9546fba180c5
Bharaniarasu <bharaniarasu.t@gmail.com>
2023-11-01 16:57:04 +0530

    productAction modified

--- client/src/components/redux/actions/productAction.js
+++ client/src/components/redux/actions/productAction.js
@@ -13,11 +13,7 @@ export const getProducts =
   (query, price, category, rating, pageNo) => async (dispatch) => {
     try {
       dispatch(productsRequest({ loading: true }));
-<<<<<<< HEAD
       let uri = `${process.env.REACT_APP_API_URL}/api/v1/products?page=${pageNo}`;
-=======
-      let uri = `/api/v1/products?page=${pageNo}`;
->>>>>>> c62796b6a723a25e6ea46d6eb6c3a86ef032e31b
       if (query) {
         uri += `&keyword=${query}`;
       }
@@ -42,11 +38,7 @@ export const getProduct = (id) => async (dispatch) => {
   try {
     dispatch(productRequest({ loading: true }));
     const { data } = await axios.get(
-<<<<<<< HEAD
       `${process.env.REACT_APP_API_URL}/api/v1/product/${id}`
-=======
-      `/api/v1/product/${id}`
->>>>>>> c62796b6a723a25e6ea46d6eb6c3a86ef032e31b
     );
 
     dispatch(productSuccess(data));
